96d64f72a59fe23f61e8ca10ad7b815bc8e84899
Vinícius Melo <vinicius.simplicio@artit.com.br>
2024-01-15 00:52:05 -0300

    remoção de comentários.

--- push_button.ino
+++ push_button.ino
@@ -3,14 +3,11 @@ int botao = 3;
 int pressionado = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(led, OUTPUT);
   pinMode(botao, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
   pressionado = digitalRead(botao);
 
   if (pressionado == HIGH) {
